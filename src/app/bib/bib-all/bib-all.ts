93b726a3cf5ab009af43b1e1a7a6dfee3ebf23c8
tomogrant <thomasolivergrant@gmail.com>
2026-03-26 19:13:13 +0100

    Cleaned up components

--- src/app/bib/bib-all/bib-all.ts
+++ src/app/bib/bib-all/bib-all.ts
@@ -1,33 +1,54 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BibService } from '../bib.service';
 import { IBib } from '../IBib';
+import { ISaga } from '../../sagas/ISaga';
+import { SagasService } from '../../sagas/sagas.service';
 
 @Component({
   selector: 'app-bibs',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './bib-all.html',
   styleUrl: './bib-all.css',
   providers: [BibService]
 })
 export class BibAll {
-  constructor(private bibService: BibService) {}
+  constructor(private bibService: BibService, private sagaService: SagasService) {}
 
-  pageTitle = 'Bibs';
+  pageTitle = 'Bibliography entries';
   bibs: IBib[] = [];
+  sagas: { [id: number]: ISaga } = {};  // Cache for saga entries
 
 ngOnInit() {
     // Fetch bibs data when the component initializes
     this.displayBibs();
   }
 
-      displayBibs(){
-        this.bibService.getBibEntries().subscribe({
-            next: receivedBibs => {
-                this.bibs = receivedBibs;
-            },
-            error: err => console.log('Error fetching bibs: ' + err)
+displayBibs(){
+      this.bibService.getBibEntries().subscribe({
+          next: receivedBibs => {
+              this.bibs = receivedBibs;
+              // Fetch all unique saga entries after bibs are loaded
+              this.fetchAllSagas();
+          },
+          error: err => console.log('Error fetching bibs: ' + err)
+      });
+  }
+
+    fetchAllSagas() {
+    // Collect all unique bibIds from all sagas
+    const allSagaIds = [...new Set(this.bibs.flatMap(bib => bib.sagaIds))];
+    allSagaIds.forEach(id => {
+      if (!this.sagas[id]) {  // Avoid duplicate fetches
+        this.sagaService.getSagaById(id).subscribe({
+          next: receivedSaga => {
+            this.sagas[id] = receivedSaga;
+          },
+          error: err => console.log('Error fetching saga: ' + err)
         });
-    }
+      }
+    });
+  }
 
 }
